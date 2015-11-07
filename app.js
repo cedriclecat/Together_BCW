d706fc29f8247bbc94388ef75d49e674d301846d
Brecht <brecht.tourlousse@student.howest.be>
2015-11-07 14:43:45 +0100

    Project Version 0.1.0

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-/**
- * Created by Brecht on 7/11/2015.
- */
-
 var express = require('express');
 var path = require('path');
+var port = process.env.PORT || 8080;
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var configDB = require('./config/connectDB.js');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var session  = require('express-session');
 
 var routes = require('./routes/index');
-//var users = require('./routes/users');
-
-// configuration ===============================================================
-
-var connectDB = require("./data/ConnectDB.js");
-mongoose.connect(connectDB); // connect to our database
+var users = require('./routes/users');
 
 var app = express();
 
@@ -24,16 +21,30 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', routes);
-//app.use('/users', users);
+// configuration ===============================================================
+mongoose.connect(configDB); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// required for passport ===============================================================
+app.use(session({ secret: 'together' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+app.use('/', routes)(app,passport);
+app.use('/users', users);
+
+// launch ===============================================================
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -41,21 +52,24 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,100 @@
-/**
- * Created by Brecht on 6/11/2015.
- */
+//var express = require('express');
+//var router = express.Router();
 
-var express = require('express');
-var router = express.Router();
+//module.exports = router;
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('landing', { title: 'Express' });
-});
+module.exports = function(app, passport) {
 
-module.exports = router;
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('landing');
+    });
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login', { message: req.flash('loginMessage') });
+    });
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+  //// locally --------------------------------
+  //app.get('/connect/local', function(req, res) {
+  //  res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+  //});
+  //app.post('/connect/local', passport.authenticate('local-signup', {
+  //  successRedirect : '/profile', // redirect to the secure profile section
+  //  failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+  //  failureFlash : true // allow flash messages
+  //}));
+
+  // =====================================
+  // FACEBOOK ROUTES =====================
+  // =====================================
+  // route for facebook authentication and login
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+  // handle the callback after facebook has authenticated the user
+  app.get('/auth/facebook/callback',
+      passport.authenticate('facebook', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+      }));
+
+  // =====================================
+  // TWITTER ROUTES ======================
+  // =====================================
+  // route for twitter authentication and login
+  app.get('/auth/twitter', passport.authenticate('twitter'));
+
+  // handle the callback after twitter has authenticated the user
+  app.get('/auth/twitter/callback',
+      passport.authenticate('twitter', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+      }));
+
+
+  // =====================================
+  // GOOGLE ROUTES =======================
+  // =====================================
+  // send to google to do the authentication
+  // profile gets us their basic information including their name
+  // email gets their emails
+  app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+  // the callback after google has authenticated the user
+  app.get('/auth/google/callback',
+      passport.authenticate('google', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+      }));
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+
+  res.redirect('/');
+}

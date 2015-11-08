ab4e68a58a5c3fc1af99866c96bcb3e691462ae3
Brecht <brecht.tourlousse@student.howest.be>
2015-11-08 14:12:55 +0100

    Schema

--- data/schema/user.js
+++ data/schema/user.js
@@ -3,21 +3,38 @@
  */
 var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
-    id:{type:Number, required:true},
-    facebook:{type:String},
-    twitter:{type:String},
-    gmail:{type:String},
-    displayname:{type:String},
-    firstname:{type:String},
-    lastname:{type:String},
-    city:{type:Number},
-    birthday:{type:Date},
-    gender:{type:String,required:true},
-    country:{type:String,required:true},
-    intrests:{type:String},
-    picture:{type:String},
-    phone:{type:Number},
-    contacts:{type:String}
+    id: { type: Number, required: true },
+    facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    twitter          : {
+        id           : String,
+        token        : String,
+        displayName  : String,
+        username     : String
+    },
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    displayname      :{type:String},
+    firstname        :{type:String},
+    lastname         :{type:String},
+    city             :{type:Number},
+    birthday         :{type:Date},
+    gender           :{type:String,required:true},
+    country          :{type:String,required:true},
+    intrests         :{type:String},
+    picture          :{type:String},
+    phone            :{type:Number},
+    contacts         :{type:String}
+
+});
 });
 
 module.exports = userSchema;

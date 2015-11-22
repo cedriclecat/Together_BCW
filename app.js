7505e40f5caa248a1f033cba406b24e383fceccb
Brecht <brecht.tourlousse@student.howest.be>
2015-11-22 16:53:12 +0100

    Update Express 4

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
 var express = require('express'),
-    app = express(),
     path = require('path'),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
+    methodOverride = require('method-override'),
     passport = require('passport'),
     session  = require('express-session'),
-    configDB = require('./config/connectDB.js');
+    logger = require('morgan'),
+    flash = require('connect-flash'),
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
+    app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/'));
-
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
 app.use(cookieParser());
 
 // configuration ===============================================================
-mongoose.connect(configDB);
+require('./config/connectDB.js');
 
 // Passport ===============================================================
 require('./config/passport.js')(passport); // pass passport for configuration
 
 app.use(session({
   secret: 'together',
-  resave: true,
-  saveUninitialized: true
+  resave: false,
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
+app.use(flash()); // connect flash to display messages
+app.use(express.static(__dirname + '/public'));
 
 // =================== Routes ===================
-
-require('./routes/index.js')(app, passport);
-
-// launch ====================================================================
-var port = process.env.PORT || 3000;
-server.listen(port);
-console.log("hello");
+app.use('/', routes);
+//require('./routes/index.js')(app,passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -75,4 +73,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-

c27a28ccf7b9c070196f41cc830b168f127915ad
Brecht <brecht.tourlousse@student.howest.be>
2015-11-24 21:14:37 +0100

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-var Events = require('../data/schema/events.js');
+var Events = require('../data/models/events');
 
+//module.exports = function (app,passport) {
 
     // =====================================
     // HOME PAGE (with login links) ========
@@ -27,148 +28,95 @@ var Events = require('../data/schema/events.js');
     });
 
     // =====================================
-    // PROFILE SECTION =====================
+    // Tweet + Geo ========================
     // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
-    router.get('/profile', function (req , res) {
-        res.render('profile',{ title: 'Profile', user: req.user});
+
+    router.get('/profile',isLoggedIn, function (req , res) {
+        res.render('profile', { title: 'Tweets' });
     });
 
     // =====================================
     // LOGIN ===============================
     // =====================================
     // show the login form
-    router.get('/login', function(req, res) {
-        res.render('login');
+    router.get('/login', function (req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.jade');/*, { message: req.flash('loginMessage') }*/
     });
 
     // process the login form
     router.post('/login', passport.authenticate('local-login', {
-        successRedirect : 'profile', // redirect to the secure profile section
-        failureRedirect : 'login' // redirect back to the signup page if there is an error
-    }));
-
-    //router.post('/login', function(req, res, next) {
-    //    passport.authenticate('local', function(err, user, info) {
-    //        if (err) { return next(err); }
-    //        // if user is not found due to wrong username or password
-    //        if (!user) {
-    //            return res.render('login', {
-    //                //you can send a message to your view
-    //                message: 'Invalid username or password'
-    //            });
-    //        }
-    //        //passport.js has a logIn user method
-    //        req.logIn(user, function(err) {
-    //            if (err) { return next(err); }
-    //
-    //            return res.redirect('/');
-    //        });
-    //    })(req, res, next);
-    //});
-
-    // =====================================
-    // FORGOTPW ============================
-    // =====================================
-    // show the login form
-    router.get('/forgotpw', function(req, res) {
-        res.render('forgotpw');
-    });
-
-    // process the signup form
-    router.post('/forgotpw', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/forgotpw' // redirect back to the signup page if there is an error
+        failureRedirect : '/login' // redirect back to the signup page if there is an error
+        //failureFlash : true // allow flash messages
     }));
 
     // =====================================
     // SIGNUP ==============================
     // =====================================
     // show the signup form
-    router.get('/register', function (req, res) {
-        res.render('register');
+    router.get('/signup', function (req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.jade' );/*, { message: req.flash('signupMessage')}*/
     });
 
 
     // process the signup form
-    router.post('/register', passport.authenticate('local-signup', {
+    router.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/register' // redirect back to the signup page if there is an error
+        failureRedirect : '/signup' // redirect back to the signup page if there is an error
+        //failureFlash : true // allow flash messages
     }));
+
+
 
     // =====================================
     // LOGOUT ==============================
     // =====================================
-
-    router.get('/logout', function(req, res){
+    router.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
 
     // =====================================
-    // 404 =================================
+    // FACEBOOK ROUTES =====================
     // =====================================
-
-    router.get('/400',function(req,res){
-        res.render('404');
-    });
-
-    // =====================================
-    // 500 =================================
-    // =====================================
-
-    router.get('/500',function(req,res){
-        res.render('500');
-    });
-
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
-
-  // =====================================
-  // FACEBOOK ROUTES =====================
-  // =====================================
-  // route for facebook authentication and login
+    // route for facebook authentication and login
     router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
 
-  // handle the callback after facebook has authenticated the user
-    router.get('/auth/facebook/callback',
-      passport.authenticate('facebook', {
-        successRedirect : '/profile',
-        failureRedirect : '/login',
-          failureFlash: true
-      }));
+    // handle the callback after facebook has authenticated the user
+    router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),function(req,res){
+        res.redirect('profile');
+    });
 
-  // =====================================
-  // TWITTER ROUTES ======================
-  // =====================================
-  // route for twitter authentication and login
+    // =====================================
+    // TWITTER ROUTES ======================
+    // =====================================
+    // route for twitter authentication and login
     router.get('/auth/twitter', passport.authenticate('twitter'));
 
-  // handle the callback after twitter has authenticated the user
-    router.get('/auth/twitter/callback', passport.authenticate('twitter', {
-        successRedirect : '/profile',
-        failureRedirect : '/login',
-        failureFlash: true
-      }));
+    // handle the callback after twitter has authenticated the user
+    router.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
 
-
-  // =====================================
-  // GOOGLE ROUTES =======================
-  // =====================================
-  // send to google to do the authentication
-  // profile gets us their basic information including their name
-  // email gets their emails
+    // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
     router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-  // the callback after google has authenticated the user
-    router.get('/auth/google/callback',
-      passport.authenticate('google', {
-        successRedirect : '/profile',
-        failureRedirect : '/login',
-          failureFlash: true
-      }));
+    //the callback after google has authenticated the user
+    router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),function(req,res){
+        res.redirect('/profile');
+    });
+
 
     // =====================================
     // API EVENTS ROUTES ===================
@@ -183,12 +131,15 @@ var Events = require('../data/schema/events.js');
     });
 
 
-// route middleware to ensure user is logged in
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
+    // route middleware to make sure a user is logged in
+    function isLoggedIn(req, res, next) {
 
-  res.redirect('/');
-}
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated())
+            return next();
+
+        // if they aren't redirect them to the home page
+        res.redirect('/');
+    };
 
 module.exports = router;

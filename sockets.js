23ce0fa7eb731d9a5b2ab1db3bceaf524402b129
wouterDumon <wouter.dumon@student.howest.be>
2015-11-28 18:11:36 +0100

    chat werkt + online mensen ( voorlopig nog niet per room )

--- sockets.js
+++ sockets.js
@@ -3,13 +3,27 @@
  */
 
 module.exports = function(io){
-
+    var allClients = [];
+   /* io.sockets.on('disconnect', function(socket) {
+        console.log('Got disconnect!');
+        allClients.splice(allClients.indexOf(socket.nick), 1);
+        socket.emit('nick',allClients);
+        socket.broadcast.emit('nick',allClients);
+    });*/
     io.sockets.on('connection', function (socket) {
-
+        socket.on('disconnect', function() {
+            console.log('Got disconnect!');
+            allClients.splice(allClients.indexOf(socket.nick), 1);
+            socket.emit('nick',allClients);
+            socket.broadcast.emit('nick',allClients);
+        });
         // Set the name property for a given client
         socket.on('nick', function(nick) {
             //  socket.set('name', nick);
-            socket.nick="anon";
+            socket.nick=nick;
+            allClients.push(socket.nick);
+            socket.emit('nick',allClients);
+            socket.broadcast.emit('nick',allClients);
         });
 
         // This should initiate rock group chat
@@ -21,7 +35,6 @@ module.exports = function(io){
                 message: data.message,
                 nick: nickname
             };
-
             socket.emit('rockgroup',payload); // show it on your own browser
             socket.broadcast.emit('rockgroup', payload); // broadcast to others
             // });

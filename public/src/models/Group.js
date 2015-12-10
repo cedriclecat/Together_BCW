5ba46728cb8a86dc79dc9e9fb5298e75b6862baa
wouterDumon <wouter.dumon@student.howest.be>
2015-12-10 13:56:14 +0100

    fix groupp js module

--- public/src/models/Group.js
+++ public/src/models/Group.js
@@ -1,12 +1,13 @@
 /**
  * Created by CedricLecat on 18/11/15.
  */
-function Group(id,name,memberids,internets,eventids,picture)
+function Group(id,name,memberids,interests,eventids,picture,chat)
 {
     this.id = id;
     this.name = name;
     this.memberids = memberids;
-    this.internets = internets;
+    this.interests = interests;
     this.eventids = eventids;
     this.picture = picture;
+    this.chat=chat;
 }

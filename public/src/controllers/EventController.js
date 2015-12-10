53839befac1d43caba2c417136e8a648334e0574
cedriclecat <cedric.lecat@student.howest.be>
2015-12-10 18:33:06 +0100

    events page
    gaat nog nie terug in

--- public/src/controllers/EventController.js
+++ public/src/controllers/EventController.js
@@ -21,7 +21,7 @@
             {
 
                 var event = new Event(
-                    data[i-1].id,
+                    inWords(i+1),
                     data[i-1].name,
                     data[i-1].description,
                     data[i-1].date,
@@ -36,7 +36,9 @@
 
                 events.push(event);
 
+
             }
+            console.log(events);
             $scope.events = events;
          }
 
@@ -47,3 +49,20 @@
    angular.module("event").controller('EventsController',['$scope',EventsController]);
 
 })();
+
+
+var a = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return;
+    var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + '' + a[n[5][1]]) + '' : '';
+
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}

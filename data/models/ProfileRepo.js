768f0f4a7577f81444d82316d5fc58afa843bef8
wouterDumon <wouter.dumon@student.howest.be>
2015-12-26 17:40:31 +0100

    data in db volledig nog createdby

--- data/models/ProfileRepo.js
+++ data/models/ProfileRepo.js
@@ -5,31 +5,38 @@ var Events = require('./events');
 
 profilerepo = (function () {
     createevent = function(data,next){
-        data.TIMESTAMP = new Date();
-        console.log(data);
-var mijnevent ={};
-        mijnevent.id='30';
-       mijnevent.name = data.etitle;
-       mijnevent.description = data.ediscription;
-       mijnevent.date = 'fg';
-       mijnevent.time = 'fg';
-       mijnevent.maxMember = data.slots;
-       var members = {};
-        mijnevent.members = members;
-       mijnevent.location = 'fg';
-       mijnevent.price = data.cost;
-       mijnevent.pictureUrl = 'fg';
-       mijnevent.tags = 'fg';
-       mijnevent.promoted= 0;
-       mijnevent.TIMESTAMP = new Date();
-       mijnevent.pictureSlider = 'fg';
-        console.log(mijnevent);        console.log(data);
-        Events.create(mijnevent, function (err) {
-            console.log("jj");
-            console.log(err);
-            if (err) { return next(err); }
-            next(mijnevent);
+
+        Events.find({},function(err,even) {
+            var mijngetal = 1;
+            even.forEach(function(e){
+                mijngetal = mijngetal +1;
+
+            });
+            data.TIMESTAMP = new Date();
+            console.log(data);
+            var mijnevent ={};
+            mijnevent.id= ''+mijngetal;
+            mijnevent.name = data.etitle;
+            mijnevent.description = data.ediscription;
+            mijnevent.date = data.date;
+            mijnevent.time = data.time;
+            mijnevent.maxMember = data.slots;
+            var members = {};
+            mijnevent.members = members;
+            mijnevent.location = data.Location;
+            mijnevent.price = data.cost;
+            mijnevent.pictureUrl = data.pictureUrl;
+            mijnevent.tags = '';
+            mijnevent.promoted= 0;
+            mijnevent.TIMESTAMP = new Date();
+            mijnevent.pictureSlider = data.pictureSlider;
+            console.log(mijnevent);        console.log(data);
+            Events.create(mijnevent, function (err) {
+                if (err) { return next(err); }
+                 next(mijnevent);
+            });
         });
+
 
     };
     return {

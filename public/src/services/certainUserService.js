cb9494329296cd356cea1f205e3fd762d401d182
wouterDumon <wouter.dumon@student.howest.be>
2016-01-19 23:04:11 +0100

    Profile Dynamisch detail

--- public/src/services/certainUserService.js
+++ public/src/services/certainUserService.js
@@ -3,9 +3,28 @@
  */
 (function(){
     var certainUserService = function($http) {
-
+        var QueryString = function () {
+            var query_string ={};
+            var query = window.location.search.substring(1);
+            var vars = query.split("&");
+            for (var i=0;i<vars.length;i++) {
+                var pair = vars[i].split("=");
+                // If first entry with this name
+                if (typeof query_string[pair[0]] === "undefined") {
+                    query_string[pair[0]] = decodeURIComponent(pair[1]);
+                    // If second entry with this name
+                } else if (typeof query_string[pair[0]] === "string") {
+                    var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+                    query_string[pair[0]] = arr;
+                    // If third or later entry with this name
+                } else {
+                    query_string[pair[0]].push(decodeURIComponent(pair[1]));
+                }
+            }
+            return query_string;
+        }();
         var byId = function(){
-            var url = '/api/profile/:_id';
+            var url = '/api/profile/:_id?id='+QueryString.id;
             return  $http({method: 'GET', url: url}).then(successCallback,errorCallback);
             function successCallback(response){
                 return response.data;

9956574de357d6a3a712304cb2a1f3816c5e6940
Brecht <brecht.tourlousse@student.howest.be>
2016-01-20 00:00:05 +0100

    chat fix

--- webpack_entrys.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by cedric on 8/12/2015.
- */
-require("./data/DataRepositorys/eventsRepo.js");
-require("./data/DataRepositorys/groupRepo.js");
-require("./data/DataRepositorys/userRepo.js");
